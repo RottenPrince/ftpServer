12ff80e3cffa0e2fb599dbc8a26bd396b26bbeea
RottenPrince <karolis0425@gmail.com>
2024-03-12 17:59:13 +0200

    done

--- src/client/client.c
+++ src/client/client.c
@@ -8,111 +8,125 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define PORT "20000"    // The port client will be connecting to 
-#define MAXDATASIZE 100 // Max number of bytes we can get at once 
-#define BUFFERSIZE 1024 // Max buffer size for sending file contents 
-#define MAX_FILES 20    // Max number of files 
-#define IPv6 "::1"      // Server address 
+#define PORT "20000"    // The port client will be connecting to
+#define MAXDATASIZE 100 // Max number of bytes we can get at once
+#define BUFFERSIZE 1024 // Max buffer size for sending file contents
+#define MAX_FILES 20    // Max number of files
+#define IPv6 "::1"      // Server address
 
 void *get_in_addr(struct sockaddr *sa);
 
+char *concat(const char *s1, const char *s2) {
+  char *result =
+      malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+  // in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
+}
+
 int main() {
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
-    int rv;
-    char s[INET6_ADDRSTRLEN];
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_UNSPEC; // Allow for any IP version 
-    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets 
-    rv = getaddrinfo(IPv6, PORT, &hints, &servinfo);
-    if (rv != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+  int sockfd;
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char s[INET6_ADDRSTRLEN];
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;     // Allow for any IP version
+  hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+  rv = getaddrinfo(IPv6, PORT, &hints, &servinfo);
+  if (rv != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+  for (p = servinfo; p != NULL;
+       p = p->ai_next) { // Loop through all the results and connect to the
+                         // first we can
+    sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (sockfd == -1) {
+      perror("client: socket");
+      continue;
     }
-    // Loop through all the results and connect to the first we can 
-    for (p = servinfo; p != NULL; p = p->ai_next) {
-        sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
-        if (sockfd == -1) {
-            perror("client: socket");
-            continue;
-        }
-        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) { // Fixed the connect() function call
-            close(sockfd);
-            perror("client: connect");
-            continue;
-        }
-        break; // If we get here, we must have connected successfully 
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) ==
+        -1) { // Fixed the connect() function call
+      close(sockfd);
+      perror("client: connect");
+      continue;
     }
+    break; // If we get here, we must have connected successfully
+  }
+  if (p == NULL) {
+    fprintf(stderr, "client: failed to connect\n");
+    return 2;
+  }
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s,
+            sizeof s);
+  printf("client: connecting to %s\n", s);
+  freeaddrinfo(servinfo); // All done with this structure
+  char cmd[MAXDATASIZE];
+  while (1) {
+    printf("> ");
+    fgets(cmd, MAXDATASIZE, stdin);
+    cmd[strcspn(cmd, "\n")] = 0; // Remove newline character
+    if (strcmp(cmd, "quit") == 0) {
+      break;
+    } else if (strcmp(cmd, "@dir") == 0) {
+      send(sockfd, cmd, strlen(cmd), 0);
+      // Receive list of files
+      char buffer[BUFFERSIZE];
+      memset(buffer, 0, BUFFERSIZE);
+      recv(sockfd, buffer, BUFFERSIZE - 1, 0);
+      printf("Files available for download:\n%s\n", buffer);
+    } else if (strncmp(cmd, "@get ", 5) == 0) {
+      send(sockfd, cmd, strlen(cmd), 0); // Send the @get command with filename
+      // Prepare to receive the file
+      char file_buffer[BUFFERSIZE];
+      memset(file_buffer, 0, BUFFERSIZE);
+      // Extract the filename from the command
+      char *filename = cmd + 5; // Skip "@get " to get the filename
+      char *filePath = "./src/client/Downloaded/";
 
-    if (p == NULL) {
-        fprintf(stderr, "client: failed to connect\n");
-        return 2;
-    }
-
-    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
-    printf("client: connecting to %s\n", s);
-    freeaddrinfo(servinfo); // All done with this structure 
-    char cmd[MAXDATASIZE];
-    while (1) {
-        printf("> ");
-        fgets(cmd, MAXDATASIZE, stdin);
-        cmd[strcspn(cmd, "\n")] = 0; // Remove newline character 
-        if (strcmp(cmd, "quit") == 0) {
-            break;
-        } else if (strcmp(cmd, "@dir") == 0) {
-            send(sockfd, cmd, strlen(cmd), 0);
-            // Receive list of files 
-            char buffer[BUFFERSIZE];
-            memset(buffer, 0, BUFFERSIZE);
-            recv(sockfd, buffer, BUFFERSIZE - 1, 0);
-            printf("Files available for download:\n%s\n", buffer);
-        } else if (strncmp(cmd, "@get ", 5) == 0) {
-            send(sockfd, cmd, strlen(cmd), 0); // Send the @get command with filename 
-            // Prepare to receive the file 
-            char file_buffer[BUFFERSIZE];
-            memset(file_buffer, 0, BUFFERSIZE);
-            // Extract the filename from the command 
-            char* filename = cmd + 5; // Skip "@get " to get the filename 
-            FILE *fp = fopen(filename, "wb");
-            if (fp == NULL) {
-                perror("Error opening file");
-                continue;
-            }
-            // Initialize to keep track of total bytes received 
-            int total_bytes_received = 0;
-            // Receive file contents 
-            int bytes_received;
-            while ((bytes_received = recv(sockfd, file_buffer, BUFFERSIZE, 0)) > 0) {
-                total_bytes_received += bytes_received; // Update total bytes received 
-                size_t written = fwrite(file_buffer, 1, bytes_received, fp);
-                if (written < (size_t)bytes_received) {
-                    perror("Error writing file");
-                    break;
-                }
-                // Check if we've received the file completely (optional, based on your protocol) 
-                if (bytes_received < BUFFERSIZE) {
-                    break; // Assume end of file if we received less than BUFFERSIZE 
-                }
-            }
-            if (bytes_received < 0) {
-                perror("recv error");
-            } else {
-                printf("File '%s' downloaded successfully, %d bytes received.\n", filename, total_bytes_received);
-            }
-            fclose(fp);
-        } else if (strcmp(cmd, "help") == 0) {
-            printf("@dir - List files available for download\n@get <filename> - Download a file\nquit - Exit the client\n");
-        } else {
-            printf("Invalid command. Type 'help' for a list of commands.\n");
+      FILE *fp = fopen(concat(filePath, filename), "wb");
+      if (fp == NULL) {
+        perror("Error opening file");
+        continue;
+      }
+      int total_bytes_received =
+          0;              // Initialize to keep track of total bytes received
+      int bytes_received; // Receive file contents
+      while ((bytes_received = recv(sockfd, file_buffer, BUFFERSIZE, 0)) > 0) {
+        total_bytes_received += bytes_received; // Update total bytes received
+        size_t written = fwrite(file_buffer, 1, bytes_received, fp);
+        if (written < (size_t)bytes_received) {
+          perror("Error writing file");
+          break;
         }
+        if (bytes_received <
+            BUFFERSIZE) { // Check if we've received the file completely
+                          // (optional, based on your protocol)
+          break; // Assume end of file if we received less than BUFFERSIZE
+        }
+      }
+      if (bytes_received < 0) {
+        perror("recv error");
+      } else {
+        printf("File '%s' downloaded successfully, %d bytes received.\n",
+               filename, total_bytes_received);
+      }
+      fclose(fp);
+    } else if (strcmp(cmd, "help") == 0) {
+      printf("@dir - List files available for download\n@get <filename> - "
+             "Download a file\nquit - Exit the client\n");
+    } else {
+      printf("Invalid command. Type 'help' for a list of commands.\n");
     }
-    close(sockfd);
-    return 0;
+  }
+  close(sockfd);
+  return 0;
 }
 
 void *get_in_addr(struct sockaddr *sa) {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
--- src/server/server.c
+++ src/server/server.c
@@ -17,125 +17,140 @@
 void *get_in_addr(struct sockaddr *sa);
 
 char *listFilesInDirectory() {
-    DIR *d;
-    struct dirent *dir;
-    char *files = malloc(BUFFERSIZE);
-    if (!files) {
-        perror("Memory allocation failed");
-        exit(EXIT_FAILURE);
+  DIR *d;
+  struct dirent *dir;
+  char *files = malloc(BUFFERSIZE);
+  if (!files) {
+    perror("Memory allocation failed");
+    exit(EXIT_FAILURE);
+  }
+  files[0] = '\0';                   // Initialize the string to be empty
+  d = opendir("./src/server/@dir/"); // Open the current directory
+  if (d) {
+    while ((dir = readdir(d)) != NULL) {
+      if (dir->d_type == DT_REG) { // Check if it's a regular file
+        strcat(files, dir->d_name);
+        strcat(files, "\n");
+      }
     }
-    files[0] = '\0'; // Initialize the string to be empty
-    d = opendir("./@dir/"); // Open the current directory
-    if (d) {
-        while ((dir = readdir(d)) != NULL) {
-            if (dir->d_type == DT_REG) { // Check if it's a regular file
-                strcat(files, dir->d_name);
-                strcat(files, "\n");
-            }
-        }
-        closedir(d);
-    }
-    return files;
+    closedir(d);
+  }
+  return files;
+}
+
+char *concat(const char *s1, const char *s2) {
+  char *result =
+      malloc(strlen(s1) + strlen(s2) + 1); // +1 for the null-terminator
+  // in real code you would check for errors in malloc here
+  strcpy(result, s1);
+  strcat(result, s2);
+  return result;
 }
 
 int main(void) {
-    int sockfd, new_fd;
-    struct addrinfo hints, *servinfo, *p;
-    struct sockaddr_storage their_addr;
-    socklen_t sin_size;
-    int yes=1;
-    char s[INET6_ADDRSTRLEN];
-    int rv;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET6; // Change this to AF_INET6 to force IPv6
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-        return 1;
+  int sockfd, new_fd;
+  struct addrinfo hints, *servinfo, *p;
+  struct sockaddr_storage their_addr;
+  socklen_t sin_size;
+  int yes = 1;
+  char s[INET6_ADDRSTRLEN];
+  int rv;
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_INET6; // Change this to AF_INET6 to force IPv6
+  hints.ai_socktype = SOCK_STREAM;
+  hints.ai_flags = AI_PASSIVE;
+  if ((rv = getaddrinfo(NULL, PORT, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+  for (p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+      perror("server: socket");
+      continue;
     }
-    for(p = servinfo; p != NULL; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-            perror("server: socket");
-            continue;
-        }
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-            perror("setsockopt");
-            exit(1);
-        }
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-        break;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+      perror("setsockopt");
+      exit(1);
     }
-    if (p == NULL)  {
-        fprintf(stderr, "server: failed to bind\n");
-        return 2;
+    if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("server: bind");
+      continue;
     }
-    freeaddrinfo(servinfo);
-    if (listen(sockfd, BACKLOG) == -1) {
-        perror("listen");
-        exit(1);
+    break;
+  }
+  if (p == NULL) {
+    fprintf(stderr, "server: failed to bind\n");
+    return 2;
+  }
+  freeaddrinfo(servinfo);
+  if (listen(sockfd, BACKLOG) == -1) {
+    perror("listen");
+    exit(1);
+  }
+  printf("server: waiting for connections...\n");
+  while (1) { // main accept() loop
+    sin_size = sizeof their_addr;
+    new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
+    if (new_fd == -1) {
+      perror("accept");
+      continue;
     }
-    printf("server: waiting for connections...\n");
-    while(1) {  // main accept() loop
-        sin_size = sizeof their_addr;
-        new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size);
-        if (new_fd == -1) {
-            perror("accept");
-            continue;
-        }
-        inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr), s, sizeof s);
-        printf("server: got connection from %s\n", s);
-    
-        char buf[BUFFERSIZE]; // Now handle each client request in a loop
-        int numbytes;
-        while((numbytes = recv(new_fd, buf, BUFFERSIZE-1, 0)) > 0) {
-            buf[numbytes] = '\0';
-            if (strcmp(buf, ".") == 0) {
-                char *fileList = listFilesInDirectory();
-                send(new_fd, fileList, strlen(fileList), 0);
-                free(fileList); // Make sure to free the allocated memory
-            } else if (strcmp(buf, "@dir") == 0) {
-                char *fileList = listFilesInDirectory("../@dir"); // Relative path from src to @dir
-                send(new_fd, fileList, strlen(fileList), 0);
-                free(fileList); // Make sure to free the allocated memory
-            } 
-            else if (strncmp(buf, "@get ", 5) == 0) {
-              // Extract the filename from the command
-              char *filename = buf + 5;
+    inet_ntop(their_addr.ss_family, get_in_addr((struct sockaddr *)&their_addr),
+              s, sizeof s);
+    printf("server: got connection from %s\n", s);
+    char buf[BUFFERSIZE]; // Now handle each client request in a loop
+    int numbytes;
+    while ((numbytes = recv(new_fd, buf, BUFFERSIZE - 1, 0)) > 0) {
+      buf[numbytes] = '\0';
+      if (strcmp(buf, ".") == 0) {
+        char *fileList = listFilesInDirectory();
+        send(new_fd, fileList, strlen(fileList), 0);
+        free(fileList); // Make sure to free the allocated memory
+      } else if (strcmp(buf, "@dir") == 0) {
+        char *fileList =
+            listFilesInDirectory(); // Relative path from src to @dir
+        send(new_fd, fileList, strlen(fileList), 0);
+        free(fileList); // Make sure to free the allocated memory
+        printf(".. Files listed ..\n");
+      } else if (strncmp(buf, "@get ", 5) == 0) {
+        // Extract the filename from the command
+        char *filename = buf + 5;
+        char *filePath = "./src/server/@dir/";
 
-              // Open the file in read-only, binary mode to prevent any modification
-              FILE *fp = fopen(filename, "rb");
-              if (fp == NULL) {
-                  perror("Failed to open file");
-                  continue;
-              }
+        FILE *fp = fopen(concat(filePath, filename),
+                         "rb"); // Open the file in read-only, binary
+                                // mode to prevent any modification
 
-              // Read file contents and send them to client
-              char file_buffer[BUFFERSIZE];
-              int bytes_read;
-              while ((bytes_read = fread(file_buffer, 1, BUFFERSIZE, fp)) > 0) {
-                  send(new_fd, file_buffer, bytes_read, 0);
-              }
-              fclose(fp); // Close the file after reading
-              printf("File sent: %s\n", filename);
-            } else {
-                // Handle other commands or close connection
-                break; // For now, break the loop if the command is not recognized
-            }
+        printf("which files is opened: %s\n", concat(filePath, filename));
+        if (fp == NULL) {
+          perror("Failed to open file");
+          continue;
         }
-        close(new_fd);  // parent doesn't need this
+        char file_buffer[BUFFERSIZE]; // Read file contents and send them to
+                                      // client
+        int bytes_read;
+        while ((bytes_read = fread(file_buffer, 1, BUFFERSIZE, fp)) > 0) {
+          printf("%s\n", file_buffer);
+          send(new_fd, file_buffer, bytes_read, 0);
+        }
+        fclose(fp); // Close the file after reading
+        printf("File sent: %s\n", filename);
+
+      } else {
+        // Handle other commands or close connection
+        break; // For now, break the loop if the command is not recognized
+      }
     }
-    return 0;
+    close(new_fd); // parent doesn't need this
+  }
+  return 0;
 }
 
 void *get_in_addr(struct sockaddr *sa) {
-    if (sa->sa_family == AF_INET) {
-        return &(((struct sockaddr_in*)sa)->sin_addr);
-    }
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in *)sa)->sin_addr);
+  }
 
-    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+  return &(((struct sockaddr_in6 *)sa)->sin6_addr);
 }
